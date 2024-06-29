5dee9e387d9ad8ab06408dc67e13945ea5ad2285
Alexandra Masnière <al90ex@gmail.com>
2024-06-29 16:39:29 +0200

    Add filters

--- OC_Testeur_logiciel_P7_Sophie_Bluel-main/FrontEnd/assets/script.js
+++ OC_Testeur_logiciel_P7_Sophie_Bluel-main/FrontEnd/assets/script.js
@@ -1,13 +1,19 @@
-//Récupération des travaux depuis l'API
-const answer = fetch("http://localhost:5678/api/works");
+//Pour attendre le chargement de la page HTML avant de lancer le script (TypeError: Cannot read properties of null (reading 'addEventListener'))
+document.addEventListener("DOMContentLoaded", function () {
+    let works = [];
 
-answer.then((response) => {
-    const works = response.json();
-    works.then((works) => {
+    //Récupération des travaux depuis l'API
+    async function getWorks() {
+        const response = await fetch("http://localhost:5678/api/works");
+        works = await response.json();
+        console.log(works);
+        showWorks(works);
+    }
+    getWorks();
 
-        //Pour parcourrir tous les travaux
+    //Création des éléments travaux
+    function showWorks(works) {
         for (let i = 0; i < works.length; i++) {
-            //Création et affichage des éléments des travaux
             const work = works[i];
             const figure = document.createElement("figure");
             const imageWork = document.createElement("img");
@@ -21,5 +27,42 @@ answer.then((response) => {
             figure.appendChild(imageWork);
             figure.appendChild(nameWork);
         }
-    })
+    }
+
+    //Tri avec les boutons
+    const buttonAll = document.querySelector(".btn-all");
+    buttonAll.addEventListener("click", function () {
+        showWorks(works);
+    });
+
+    const buttonObj = document.querySelector(".btn-obj");
+    buttonObj.addEventListener("click", function () {
+        const workObj = works.filter(function (work) {
+            return work.categoryId === 1;
+        })
+        console.log(workObj);
+        // Efface le contenu de la balise body et donc l’écran
+        document.querySelector(".gallery").innerHTML = '';
+        showWorks(workObj);
+    });
+
+    const buttonApt = document.querySelector(".btn-apt");
+    buttonApt.addEventListener("click", function () {
+        const workApt = works.filter(function (work) {
+            return work.categoryId === 2;
+        })
+        console.log(workApt);
+        document.querySelector(".gallery").innerHTML = '';
+        showWorks(workApt);
+    });
+
+    const buttonHr = document.querySelector(".btn-hr");
+    buttonHr.addEventListener("click", function () {
+        const workHr = works.filter(function (work) {
+            return work.categoryId === 3;
+        })
+        console.log(workHr);
+        document.querySelector(".gallery").innerHTML = '';
+        showWorks(workHr);
+    });
 });
